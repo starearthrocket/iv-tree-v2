54bfb603a030891b1c805d5bcff463220b93dd79
starearthrocket <50158128@stu.gbmc.ac.uk>
2026-08-24 18:43:29 +0100

    Register report models with admin

--- reports/admin.py
+++ reports/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ProgressUpdate, TreeReport
+
+
+admin.site.register(TreeReport)
+admin.site.register(ProgressUpdate)
